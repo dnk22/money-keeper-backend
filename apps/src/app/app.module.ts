2bb3299aa78acb11bf176d22319411687bce9acd
Duy Nguyen <duynk198@gmail.com>
2022-08-19 00:04:40 +0700

    feat : add mongo db

--- apps/src/app/app.module.ts
+++ apps/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from '../environments/environment';
+import { UserModule } from './modules/user/user.module';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(environment.db_uri), UserModule],
   controllers: [],
   providers: [],
 })
--- apps/src/environments/environment.ts
+++ apps/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  globalPrefix : 'api/v1'
+  globalPrefix : 'api/v1',
+  db_uri : 'mongodb+srv://duynk:khanhduy1998@moneykeeper.riybgqp.mongodb.net/?retryWrites=true&w=majority'
 };
